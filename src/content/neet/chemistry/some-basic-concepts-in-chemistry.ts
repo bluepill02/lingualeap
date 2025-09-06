3c8423729e036f0aa0273f49e4d82fd909c6669b
bluepill <smvinu69@gmail.com>
2025-09-06 06:04:41 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/content/neet/chemistry/some-basic-concepts-in-chemistry.ts
@@ -0,0 +1,21 @@
+
+import type { NeetModule } from '@/lib/types';
+
+export const someBasicConceptsInChemistry: NeetModule = {
+    id: 'neet-chemistry-some-basic-concepts-in-chemistry',
+    title: 'Chemistry - Some Basic Concepts in Chemistry',
+    chapter: 'Some Basic Concepts in Chemistry',
+    subject: 'Chemistry',
+    learningObjectives: [],
+    prerequisites: [],
+    syllabusMapping: [],
+    conceptNotes: [],
+    workedExamples: [],
+    mcqs: [],
+    assertionReasons: [],
+    matchTheColumns: [],
+    keyTakeaways: [],
+    mnemonics: [],
+    neetTips: [],
+    validationReport: []
+};
