b50db8fbae5979cf8f7d5709558f6eaf8e6da902
bluepill <smvinu69@gmail.com>
2025-09-06 10:34:29 +0000

    add left and right padding for  NEET module pages

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -167,8 +167,8 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
           </DropdownMenu>
         </header>
 
-        <main className="flex-1 p-4 md:p-6 lg:p-8">
-            <div className="container mx-auto">
+        <main className="flex-1">
+            <div className="container mx-auto px-4 md:px-6 lg:px-8 py-6 md:py-8 lg:py-10">
                 {children}
             </div>
         </main>
